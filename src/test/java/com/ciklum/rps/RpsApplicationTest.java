26a7ae053e0885d5dd97d09df3dcc53e88457c7b
rafaelpriegoporcuna <rafapriego@gmail.com>
2020-07-23 19:00:48 +0200

    Initial commit with basic spring boot web app with thymeleaf

---
+++ src/test/java/com/ciklum/rps/RpsApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ciklum.rps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
