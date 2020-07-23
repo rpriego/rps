0c939ae0c1318ef5efd4fd92dc633a31792be75a
rafaelpriegoporcuna <rafapriego@gmail.com>
2020-07-23 19:52:20 +0200

    Passed tests for the shape strategy (or factory)

--- src/main/java/com/ciklum/rps/services/IShapeStrategy.java
+++ src/main/java/com/ciklum/rps/services/IShapeStrategy.java
@@ -5,13 +5,15 @@ import com.ciklum.rps.domain.Shape;
 import java.util.Random;
 import java.util.function.Supplier;
 
+import static com.ciklum.rps.domain.Shape.ROCK;
+
 public interface IShapeStrategy extends Supplier<Shape> {
 
     static IShapeStrategy random(Random random) {
-        return null;
+        return () -> Shape.values()[random.nextInt(Shape.values().length)];
     }
 
     static IShapeStrategy rock() {
-        return null;
+        return () -> ROCK;
     }
 }
